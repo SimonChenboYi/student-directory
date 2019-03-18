017cba0defb3c29400c432b4d4af87c8804bbcf1
Chenbo Yi <yichenbo@outlook.com>
2019-03-18 17:28:13 +0000

    Step8_exercise5: Ask user to input more information: hobbies, country of birth, height

--- directory.rb
+++ directory.rb
@@ -2,21 +2,36 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+
   # creat an empty array
   students = []
+
   # get the fitst name
   name = gets.chomp
+
   # while (loop) the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
+    puts "Please enter the hobby of the student"
+    hobby = gets.chomp
+    puts "Please enter the Country of birth of the student"
+    country_of_birth = gets.chomp
+    puts "Please enter the height of the student"
+    height = gets.chomp
+
+    students << { name: name, cohort: :november, hobby: hobby,
+                  country_of_birth: country_of_birth, height: height }
     puts "Now we have #{students.count} students"
+
     # get another name from user
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
     name = gets.chomp
   end
+
   # return the array of students
   students
 end
+
 # define methods for print header, names and total number
 def print_header
   puts "The students of Villains Academy"
@@ -25,11 +40,19 @@ end
 
 def print(students)
   puts "Please enter the initial letter of names to print or just hit return to print all "
+  # get user input about print choice
   specific_letter = gets.chomp
+  # while loop the students array
   index = 0
   while index < students.size
+    # use if statement to accomodate user choice on printing
     if (specific_letter.empty? || students[index][:name][0] == specific_letter) && students[index][:name].size < 12
-    puts "#{index + 1}: #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+
+    # Print addtional infomation of the students
+    puts "#{index + 1}: #{students[index][:name]}" +
+      " (#{students[index][:cohort]} cohort, height: #{students[index][:height]})" +
+      "from #{students[index][:country_of_birth]}" +
+      " likes #{students[index][:hobby]}"
     end
     index += 1
   end
@@ -38,8 +61,10 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
 # Ask user input by invoking the input_students methods, assign the output to a variable
 students = input_students
+
 # Print header, students and total number by invoking the pre-defined methods
 print_header
 print(students)

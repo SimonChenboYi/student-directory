52653ee5a7cab2042d889d5e32cf6659efdbf3fc
Chenbo Yi <yichenbo@outlook.com>
2019-03-18 21:05:39 +0000

    Step8_exercise7: Ask for multiple inputs with default values in place, convert the input from string to symbol where applicable,define check_list and update_list methods for updating list and correcting typo

--- directory.rb
+++ directory.rb
@@ -5,20 +5,31 @@ def input_students
 
   # creat an empty array
   students = []
-
   # get the fitst name
   name = gets.chomp
 
   # while (loop) the name is not empty, repeat this code
-  while !name.empty? do
+  until name.empty? do
+    puts "Please enter the cohort of the student"
+
+    #convert to symbol
+    cohort = gets.chomp.to_sym
+    # set up default value
+    cohort = :november if cohort.empty?
+
     puts "Please enter the hobby of the student"
     hobby = gets.chomp
+    hobby = '???' if hobby.empty?
+
     puts "Please enter the Country of birth of the student"
     country_of_birth = gets.chomp
+    country_of_birth = '???' if country_of_birth.empty?
+
     puts "Please enter the height of the student"
     height = gets.chomp
+    height = '???' if height.empty?
 
-    students << { name: name, cohort: :november, hobby: hobby,
+    students << { name: name, cohort: cohort, hobby: hobby,
                   country_of_birth: country_of_birth, height: height }
     puts "Now we have #{students.count} students"
 
@@ -27,9 +38,49 @@ def input_students
     puts "To finish, just hit return twice"
     name = gets.chomp
   end
-
   # return the array of students
   students
+end
+
+# Print the list with index to have a visual check
+def check_list(students)
+  students.each_with_index{|student,index| puts "#{index} : #{student}"}
+end
+
+# Update the list and correct the typo"
+def update_list(students)
+  # check the list first  when call the update_list
+  check_list(students)
+  puts "Please enter index need to update"
+  puts "To finish update, just hit return twice"
+
+  # update according to the index, key and updated value
+  index = gets.chomp
+
+  until index.empty?
+    puts "Please enter the category you want to update"
+    key = gets.chomp.to_sym
+    # check whether the input key is valid,otherwise ask user input agian
+    while students[index.to_i][key].nil?
+      puts "input does not match any exsiting category, please re-enter"
+      key = gets.chomp.to_sym
+    end
+    # update value
+    puts "Please enter the value you want to update"
+    value = gets.chomp
+    # if the current value is a symbol then convert the updated value to symbol
+    value = value.to_sym if students[index.to_i][key].is_a?(Symbol)
+    students[index.to_i][key] = value
+
+    # print the updated hash
+    puts " updated :#{students[index.to_i]}"
+
+    # loop the update until no update needed
+    puts "Please enter index need to update"
+    puts "To finish update, just hit return twice"
+
+    index = gets.chomp
+  end
 end
 
 # define methods for print header, names and total number
@@ -39,9 +90,10 @@ def print_header
 end
 
 def print(students)
-  puts "Please enter the initial letter of names to print or just hit return to print all "
+  puts "Please enter the first letter of name or just hit return to print all "
   # get user input about print choice
   specific_letter = gets.chomp
+
   # while loop the students array
   index = 0
   while index < students.size
@@ -62,8 +114,11 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-# Ask user input by invoking the input_students methods, assign the output to a variable
+# Invoking the input_students methods, assign the output to a variable
 students = input_students
+
+# Invoke update_list to see whether there is anything need to update
+update_list(students)
 
 # Print header, students and total number by invoking the pre-defined methods
 print_header

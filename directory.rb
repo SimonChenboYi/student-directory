c60c6fa8506850f186ecd5445d50d4959bba9041
Chenbo Yi <yichenbo@outlook.com>
2019-03-19 13:16:45 +0000

    Add 'load from file' function to load list from csv file

--- directory.rb
+++ directory.rb
@@ -39,6 +39,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -63,6 +64,17 @@ def save_students
   file.close
 end
 
+def load_students
+  # option 4. load the list from students.csv
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line| # readlines
+    name, cohort = line.chomp.split(",") # parallel assignment
+    @students << { name: name, cohort: cohort.to_sym }
+  end
+  file.close
+end
+
+
 def process(selection)
   case selection
   when "1"
@@ -71,6 +83,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     # terminate the program
     exit
